39e90a41e5cff2c1807be374414436237958e432
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-18 17:02:08 +0900

    update

--- backbones/stackedHourglass.py
+++ backbones/stackedHourglass.py
@@ -5,3 +5,19 @@ import torch.nn.functional as F
 __all__ = ["HourglassNet", "hg"]
 
 
+class Bottleneck2D(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck2D, self).__init__()
+
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 2, kernel_size=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+    
+
+    
